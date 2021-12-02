e86508133b5d1c53255345adb1961aae27a9c786
Dewald Haasbroek <73649603+Dewald844@users.noreply.github.com>
2021-12-02 20:18:37 +0200

    Cleanup

--- Day2_2021/Program.fs
+++ Day2_2021/Program.fs
@@ -1,15 +1,7 @@
 // Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
 
-type Position = {
-  Horizontal : int
-  Depth : int
-  Aim : int
-}
-
-type Move =
-  | Forward of int
-  | Down of int
-  | Up of int
+type Position = { Horizontal : int; Depth : int; Aim : int}
+type Move = Forward of int | Down of int | Up of int
 
 module Move =
   let fromString (s,int) =
@@ -19,16 +11,6 @@ module Move =
     | "down", x    -> Down x
     | _ -> failwith "Unexpected input"
     
-let testList =
-  [
-    "forward 5"
-    "down 5"
-    "forward 8"
-    "up 3"
-    "down 8"
-    "forward 2"
-  ]
-    
 let inputLines =
   System.IO.File.ReadLines "./input.txt" 
   |> Seq.toList
@@ -37,31 +19,18 @@ let inputLines =
   
 let calculate position move =
   match move with
-  | Forward x -> { position with Horizontal = position.Horizontal + x
-                                 Depth = (position.Aim * x) + position.Depth }
+  | Forward x -> { position with Horizontal = position.Horizontal + x ;Depth = (position.Aim * x) + position.Depth }
   | Up x      -> { position with Aim = position.Aim - x }
   | Down x    -> { position with Aim = position.Aim + x }
   
-let initPosition =
-  { Horizontal = 0
-    Depth = 0
-    Aim = 0 }
+let initPosition = { Horizontal = 0; Depth = 0; Aim = 0 }
   
 let result =   
   inputLines
   |> List.map Move.fromString
   |> List.fold calculate initPosition
- 
-let testResult =   
-  testList
-  |> List.map (fun x -> x.Split " ")
-  |> List.map (fun x -> x.[0], int x.[1])
-  |> List.map Move.fromString
-  |> List.fold calculate initPosition
- 
-let testTotal = testResult.Horizontal * testResult.Depth
+
 let resultTotal = result.Horizontal * result.Depth
   
 printfn "RESULT : %A" resultTotal
-printfn "TEST : %i" testTotal
   
