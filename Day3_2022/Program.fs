1f7974fb8a1e6cf5e79f2b15a620856e156d5789
Dewald Haasbroek <dewald.haasbroek@agrico.co.za>
2022-12-06 16:12:55 +0200

    Day 4 complete

--- Day3_2022/Program.fs
+++ Day3_2022/Program.fs
@@ -1,7 +1,8 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
 open System.IO
 
-let inputL () = File.ReadLines("../../../input.txt")
+let inputL () =
+   File.ReadLines("../../../input.txt")
 
 let alphabet =  // There must be easier way to do this but all that i can think of now
    [
@@ -11,28 +12,22 @@ let alphabet =  // There must be easier way to do this but all that i can think 
 
 let charScore c = (alphabet |> List.findIndex (fun char -> char = c)) + 1
 
-// Intersect Explanation
-// http://nowcoding.net/fsharp-common-intersection-of-a-sequence-of-sequences/
-
-// Reduce Explanation
-// https://bradcollins.com/2015/05/22/f-friday-the-reduce-function/
-
 let findCommon s =
    s
-   |> Seq.map Set.ofSeq    // get set of chars
-   |> Seq.reduce Set.intersect  // get intersections
-   |> Set.toSeq                         // return seq of chars
+   |> Seq.map Set.ofSeq
+   |> Seq.reduce Set.intersect
+   |> Set.toSeq
 
 let part1 =
    inputL ()
-   |> Seq.collect (Seq.splitInto 2 >> findCommon) // split in half and find common chars
-   |> Seq.sumBy charScore                                   // get score of common char
+   |> Seq.collect (Seq.splitInto 2 >> findCommon)
+   |> Seq.sumBy charScore
 
 let part2 =
    inputL ()
-   |> Seq.chunkBySize 3    // group 3 string together
-   |> Seq.collect findCommon // find the common in the 3 strings
-   |> Seq.sumBy charScore              // get score of common char
+   |> Seq.chunkBySize 3
+   |> Seq.collect findCommon
+   |> Seq.sumBy charScore
 
 printf $"Sum of part 1 data : {part1}\n"
 printf $"Sum of part 2 data : {part2}\n"
---
+++ Day4_2022/Program.fs
@@ -0,0 +1,42 @@
+﻿
+type SectionRange = { start: int; end': int }
+
+let parseSectionRange (input: string) =
+    let parts = input.Split '-'
+    { start = int parts.[0]; end' = int parts.[1] }
+
+let contains (range1: SectionRange) (range2: SectionRange) =
+    range1.start <= range2.start && range1.end' >= range2.end'
+
+let overlaps (range1: SectionRange) (range2: SectionRange) =
+    range1.end' >= range2.start && range1.start <= range2.end'
+
+let inputL () =
+   System.IO.File.ReadLines("../../../input.txt") |> List.ofSeq
+
+let main1 () =
+    let result =
+        inputL ()
+        |> List.map (fun s ->
+            let ranges = s.Split ',' |> Array.map parseSectionRange
+            contains ranges.[0] ranges.[1] || contains ranges.[1] ranges.[0]
+        )
+        |> List.filter id
+        |> List.length
+
+    printfn $"There are %d{result} pairs where one range fully contains the other."
+
+let main2 () =
+    let result =
+        inputL ()
+        |> List.map (fun s ->
+            let ranges = s.Split ',' |> Array.map parseSectionRange
+            overlaps ranges.[0] ranges.[1]
+        )
+        |> List.filter id
+        |> List.length
+
+    printfn $"There are %d{result} pairs where one range fully overlaps each other."
+
+main1 ()
+main2 ()
