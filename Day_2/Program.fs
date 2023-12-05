f1b754256e141d9f25d1a41c6775502c32425300
Dewald Haasbroek <dewald.haasbroek@agrico.co.za>
2023-12-05 13:21:06 +0200

    Day 2 complete

--- Day_2/Program.fs
+++ Day_2/Program.fs
@@ -12,35 +12,116 @@ let test  = [
 
 type Count = { Red: int; Green: int; Blue: int }
 
-type Game = { GameNumber: int; Cubes: Count }
+type Game = { GameNumber: int; Round: List<Count> }
 
 let parseLine (line: string) =
-   let gameNumber = line.Split(':').[0].Split(' ')[1] |> int
-   let cubes = line.Split(':').[1].Split(';')
-   let count =
-      cubes
-      |> Array.map (fun x -> x.Trim().Split(", "))
+   let gameNumber = line.Split(":").[0].Split(" ")[1] |> int
+   let cubes =
+      line.Split(": ").[1].Trim().Split("; ")
       |> Array.map (fun x ->
-         let first = x[0].Split(' ')
-         let second = x[1].Split(' ')
-         [
-            (first.[1], int first.[0])
-            (second.[1], int second.[0])
-         ] |> List.toArray
+         let round = x.Trim().Split(", ")
+         let redCubes   = round |> Array.filter (fun c -> (c.Split(" ")[1]).Trim() = "red" )
+         let greenCubes = round |> Array.filter (fun c -> (c.Split(" ")[1]).Trim() = "green" )
+         let blueCubes  = round |> Array.filter (fun c -> (c.Split(" ")[1]).Trim() = "blue" )
+         let countCubes (cubes: string[]) =
+            cubes
+            |> Array.map (fun x -> x.Split(" ")[0] |> int)
+            |> Array.sum
+
+         { Red = redCubes |> countCubes; Green = greenCubes |> countCubes; Blue = blueCubes |> countCubes }
       )
-      |> Array.concat
-   let red = count |> Array.filter (fun (x, _) -> x = "red") |> Array.map snd |> Array.sum
-   let green = count |> Array.filter (fun (x, _) -> x = "green") |> Array.map snd |> Array.sum
-   let blue = count |> Array.filter (fun (x, _) -> x = "blue") |> Array.map snd |> Array.sum
-   { GameNumber = gameNumber; Cubes = { Red = red; Green = green; Blue = blue } }
+
+   {
+      GameNumber = gameNumber
+      Round = cubes |> Array.toList
+   }
 
 let countPossibleGames (games : List<Game>) : List<int> =
    games
-   |> List.filter (fun x -> x.Cubes.Red <= 12 && x.Cubes.Green <= 13 && x.Cubes.Blue <= 14)
+   |> List.filter (fun x ->
+      x.Round
+      |> List.forall (fun round ->
+         round.Red <= 12 && round.Green <= 13 && round.Blue <= 14
+         )
+      )
    |> List.map (fun x -> x.GameNumber)
 
-let part1Test = test |> List.map parseLine |> countPossibleGames |> List.sum
+let calculatePart1 (input : List<string>) =
+   input
+   |> List.map parseLine
+   |> countPossibleGames
+   |> List.sum
 
-printf "---------------------\n"
+//============== PART 2 ==============
+
+let test2 = [
+   "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
+   "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"
+   "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"
+   "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red"
+   "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
+]
+
+let parseLine2 (line: string) =
+   let cubes =
+      line.Split(": ").[1].Trim().Split("; ")
+      |> Array.map(fun x -> x.Trim().Split(", "))
+      |> Array.concat
+      |> fun l -> printf $"Game : %A{l}"; l
+
+   let lowestCubes =
+
+         let lowestRed   =
+            cubes
+            |> Array.filter (fun x -> x.Split(" ").[1] = "red")
+            |> Array.map (fun x -> x.Trim().Split(" ")[0] |> int)
+            |> Array.sortByDescending id
+            |> Array.tryHead
+            |> Option.defaultValue 1
+
+         let lowestGreen =
+            cubes
+            |> Array.filter (fun c -> (c.Split(" ").[1]) = "green" )
+            |> Array.map (fun x -> x.Split(" ")[0] |> int)
+            |> Array.sortByDescending id
+            |> Array.tryHead
+            |> Option.defaultValue 1
+
+         let lowestBlue  =
+            cubes
+            |> Array.filter (fun c -> (c.Split(" ").[1]) = "blue" )
+            |> Array.map (fun x -> x.Split(" ")[0] |> int)
+            |> Array.sortByDescending id
+            |> Array.tryHead
+            |> Option.defaultValue 1
+
+         printf $"Lowest red: {lowestRed}, Lowest green: {lowestGreen}, Lowest blue: {lowestBlue}\n"
+
+         (lowestRed, lowestGreen, lowestBlue)
+
+
+   lowestCubes
+   |> fun (r, g, b) -> r * g * b
+
+let calculatePart2 (input : List<string>) =
+   input
+   |> List.map parseLine2
+   |> List.sum
+
+let part1 = calculatePart1 inputL
+let part1Test = calculatePart1 test
+
+let part2 = calculatePart2 inputL
+let part2Test = calculatePart2 test2
+
+
+printf "===============================\n"
 printf $"Part 1 test : {part1Test}\n"
+printf $"Part 1 : {part1}\n"
+printf "===============================\n"
+printf $"Part 2 test : {part2Test}\n"
+printf $"Part 2 : {part2}\n"
+printf "===============================\n"
+
+
 
