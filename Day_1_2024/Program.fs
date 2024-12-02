47d1336c22282851b8976cd346145a62221eb64a
Dewald Haasbroek <dewald.haasbroek@agrico.co.za>
2024-12-02 08:18:51 +0200

    Day one complete and Day 2 part 1

---
+++ Day_1_2024/Program.fs
@@ -0,0 +1,49 @@
+﻿let inputL = System.IO.File.ReadLines("./input.txt") |> List.ofSeq
+
+let testData =
+   """3   4
+4   3
+2   5
+1   3
+3   9
+3   3"""
+
+let testL =
+   inputL
+   |> List.map (fun x -> x.Split("   ") |> Array.map int |> List.ofArray)
+   |> List.map (fun a -> (a[0], a[1]))
+
+
+let calculateDifference (a, b) = abs(a - b)
+
+// Part 1
+let result1 inputL =
+   let rL = inputL |> List.map (fun (r, l) -> r) |> List.sortDescending
+   let lL = inputL |> List.map (fun (r, l) -> l) |> List.sortDescending
+
+   let rec loop rL lL acc =
+      match rL, lL with
+      | [], [] -> acc
+      | r::rs, l::ls -> loop rs ls (acc + calculateDifference(r, l))
+      | _, _ -> failwith "Invalid input"
+
+   loop rL lL 0
+
+// Part 1
+result1 testL |> printfn "%A"
+
+let result2 inputL =
+   let rL = inputL |> List.map (fun (r, l) -> r)
+   let lL = inputL |> List.map (fun (r, l) -> l)
+
+   lL
+   |> List.map (fun l ->
+      rL
+      |> List.filter (fun r -> r = l)
+      |> List.length
+      |> fun count -> l * count
+   )
+   |> List.sum
+
+// Part 2
+result2 testL |> printfn "%A"
---
+++ Day_2_2024/Program.fs
@@ -0,0 +1,34 @@
+﻿let testInput =
+   """7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9""".Split("\n")
+
+let input = System.IO.File.ReadAllLines "./input.txt"
+
+let prepareInput (input : string array) =
+   input
+   |> Array.map (fun x -> x.Split(" ") |> Array.map int |> List.ofArray)
+   |> Array.map (fun a -> (a[0], a[1], a[2], a[3], a[4]))
+
+
+let determineSafety (a, b, c, d, e) =
+   let ab = a - b
+   let bc = b - c
+   let cd = c - d
+   let de = d - e
+
+   if ab > 0 && ab <= 2 then
+      bc <= 2 && cd <= 2 && de <= 2
+   elif ab < 0 && ab >= -2 then
+      bc >= -2 && cd >= -2 && de >= -2
+   else
+      false
+
+
+let result1 input = input |> prepareInput |> Array.filter determineSafety |> Array.length
+
+// Part 1
+result1 input |> printfn "%A"
