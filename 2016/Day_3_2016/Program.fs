081db5d19450aa4f155b94150da3453b81da946e
Dewald Haasbroek <dewald.haasbroek@agrico.co.za>
2024-12-18 08:45:39 +0200

    2016 Day 3 complete

---
+++ 2016/Day_3_2016/Program.fs
@@ -0,0 +1,34 @@
+﻿
+let checkTriangle (a, b, c) = a + b > c && a + c > b && b + c > a
+
+let parseLine (line: string) =
+   line.Split("  ")
+   |> Array.map _.Trim()
+   |> Array.filter (fun s -> s <> "" && s <> " ")
+   |> Array.map int
+
+let parseInput (input: string) =
+   input.Split("\n")
+   |> Array.map parseLine
+
+let countValidTriangles (input: string) =
+   input
+   |> parseInput
+   |> Array.filter (fun l -> checkTriangle (l[0], l[1], l[2]))
+   |> Array.length
+
+let countVerticleValidTriangles (input: string) =
+   input
+   |> parseInput
+   |> Array.chunkBySize 3
+   |> Array.map (fun l -> l |> Array.transpose |> Array.map (fun x -> x |> Array.toList) |> Array.map (fun l -> checkTriangle (l.[0], l.[1], l.[2])))
+   |> Array.map (fun l -> l |> Array.filter id |> Array.length)
+   |> Array.sum
+
+let inputString = System.IO.File.ReadAllText "./input.txt"
+
+let result1 = countValidTriangles inputString
+printfn "Test result: %A" result1
+
+let result2 = countVerticleValidTriangles inputString
+printfn "Test result: %A" result2
